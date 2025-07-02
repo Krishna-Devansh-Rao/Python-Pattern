f93c06dad56a5d67359f9c765f457e30c3273490
KRISHNA DEVANSH RAO <100765720+Krishna-Devansh-Rao@users.noreply.github.com>
2025-07-02 14:31:03 +0530

    Create Angl.py

---
+++ Angl.py
@@ -0,0 +1,43 @@
+#import turtle
+
+#Square
+
+# t = turtle.Turtle()
+# t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+# t.forward(100)
+# t.right(90)
+
+
+
+
+#Eight ( 8 )
+
+#import turtle
+#t = turtle.Turtle()
+#t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+#t.circle(100)
+#t.left(180)
+#t.circle(100)
+#t.right(180)
+#t.circle(100)
+
+
+#Triangle inside Circle 
+
+# import turtle
+# t = turtle.Turtle()
+# t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+# t.circle(100)
+# t.left(55)
+# t.forward(120)
+# t.left(125)
+# t.forward(120)
+# t.left(118)
+# t.forward(120)
+       
